782ab789346007f620f4f9fe1863d83777272b0b
tranleminh <tranleminh.ams@gmail.com+lmtran@etud.insa-toulouse.fr>
2019-05-14 14:27:35 +0200

    Clean folder

--- PartieVHDL/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/th_tran/4IR Projet SysInfo/Projet_System_Info/PartieVHDL/BdR_test_isim_beh.exe" -prj "/home/th_tran/4IR Projet SysInfo/Projet_System_Info/PartieVHDL/BdR_test_beh.prj" "work.BdR_test" 
