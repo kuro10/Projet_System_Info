bdb7d7f055a168df6a969df09cb687b7e507fe1a
tranleminh <tranleminh.ams@gmail.com+lmtran@etud.insa-toulouse.fr>
2019-05-20 15:16:04 +0200

    Add Gest_alea.vhd

--- vhdl_src/Gest_alea.vhd
+++ vhdl_src/Gest_alea.vhd
@@ -30,24 +30,96 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Gest_alea is
-    Port ( inA : in  STD_LOGIC_VECTOR(15 downto 0);
-           inOP : in  STD_LOGIC_VECTOR(7 downto 0);
-           inB : in  STD_LOGIC_VECTOR(15 downto 0);
-           inC : in  STD_LOGIC_VECTOR(15 downto 0);
-           outA : out  STD_LOGIC_VECTOR(15 downto 0);
-           outOP : out  STD_LOGIC_VECTOR(7 downto 0);
-           outB : out  STD_LOGIC_VECTOR(15 downto 0);
-           outC : out  STD_LOGIC_VECTOR(15 downto 0));
+    Port ( in_LiOP : in  STD_LOGIC_VECTOR(7 downto 0);
+           in_LiB : in  STD_LOGIC_VECTOR(15 downto 0);
+           in_LiC : in  STD_LOGIC_VECTOR(15 downto 0);
+           in_DiOP : in  STD_LOGIC_VECTOR(7 downto 0);
+			  in_DiA : in  STD_LOGIC_VECTOR(15 downto 0);
+           in_ExOP : in  STD_LOGIC_VECTOR(7 downto 0);
+           in_ExA : in  STD_LOGIC_VECTOR(15 downto 0);
+           in_memOP : in  STD_LOGIC_VECTOR(7 downto 0);
+			  in_memA : in  STD_LOGIC_VECTOR(15 downto 0);
+			  alea : out STD_LOGIC
+			  );
 end Gest_alea;
 
 architecture Behavioral of Gest_alea is
 
+signal res : STD_LOGIC := '0';
+
 begin
-
-	outA <= inA;
-	outOP <= inOP;
-	outB <= inB;
-	outC <= inC;
-
+	process
+	begin
+	
+	--Case ADD, MUL, SUB--
+	if in_LiOP = x"01" or in_LiOP = x"02" or in_LiOP = x"03" then
+		--DIEX Level--
+		if in_DiOP = x"01" or in_DiOP = x"02" or in_DiOP = x"03" or in_DiOP = x"05" or in_DiOP = x"06" or in_DiOP = x"07" then
+			if in_DiA = in_LiB or in_DiA = in_LiC then
+				res <= 1;
+			end if;
+		end if;
+		--ExMem level--
+		if in_ExOP = x"01" or in_ExOP = x"02" or in_ExOP = x"03" or in_ExOP = x"05" or in_ExOP = x"06" or in_ExOP = x"07" then
+			if in_ExA = in_LiB or in_ExA = in_LiC then
+				res <= 1;
+			end if;
+		end if;
+		--MemRE Level--
+		if in_memOP = x"01" or in_memOP = x"02" or in_memOP = x"03" or in_memOP = x"05" or in_memOP = x"06" or in_memOP = x"07" then
+			if in_memA = in_LiB or in_memA = in_LiC then
+				res <= 1;
+			end if;
+		end if;
+	end if;
+	
+	--Case COP--
+	if in_LiOP = x"05" then
+		--DIEX Level--
+		if in_DiOP = x"01" or in_DiOP = x"02" or in_DiOP = x"03" or in_DiOP = x"05" or in_DiOP = x"06" or in_DiOP = x"07" then
+			if in_DiA = in_LiB then
+				res <= 1;
+			end if;
+		end if;
+		--ExMem level--
+		if in_ExOP = x"01" or in_ExOP = x"02" or in_ExOP = x"03" or in_ExOP = x"05" or in_ExOP = x"06" or in_ExOP = x"07" then
+			if in_ExA = in_LiB then
+				res <= 1;
+			end if;
+		end if;
+		--MemRE Level--
+		if in_memOP = x"01" or in_memOP = x"02" or in_memOP = x"03" or in_memOP = x"05" or in_memOP = x"06" or in_memOP = x"07" then
+			if in_memA = in_LiB then
+				res <= 1;
+			end if;
+		end if;
+	end if;
+	
+	--Case STORE--
+	if in_LiOP = x"08" then
+		--DIEX Level--
+		if in_DiOP = x"01" or in_DiOP = x"02" or in_DiOP = x"03" or in_DiOP = x"05" or in_DiOP = x"06" or in_DiOP = x"07" then
+			if in_DiA = in_LiC then
+				res <= 1;
+			end if;
+		end if;
+		--ExMem level--
+		if in_ExOP = x"01" or in_ExOP = x"02" or in_ExOP = x"03" or in_ExOP = x"05" or in_ExOP = x"06" or in_ExOP = x"07" then
+			if in_ExA = in_LiC then
+				res <= 1;
+			end if;
+		end if;
+		--MemRE Level--
+		if in_memOP = x"01" or in_memOP = x"02" or in_memOP = x"03" or in_memOP = x"05" or in_memOP = x"06" or in_memOP = x"07" then
+			if in_memA = in_LiC then
+				res <= 1;
+			end if;
+		end if;
+	end if;	
+	
+	end process;
+	
+	alea <= res;
+	
 end Behavioral;
 
